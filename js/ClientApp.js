85138bae9d172d2e1dc07bd0cc904f04d23f1a83
mcamara <mcamara@qbp.com>
2016-12-01 11:21:19 -0600

    Hour 2 Add Webpack

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,29 +1,23 @@
-var div = React.DOM.div
-var h1 = React.DOM.h1
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyTitle from './MyTitle';
 
-var MyTitle = React.createClass({
-  render: function () {
-    return (
-      div(null,
-        h1({ style: { color: this.props.color } }, this.props.title)
-      )
-    )
-  }
-})
+var div = React.DOM.div
 
 var MyTitleFactory = React.createFactory(MyTitle)
 
 var MyFirstComponent = React.createClass({
-  render: function () {
-    return (
-      div(null,
-        MyTitleFactory({title: 'props are the best', color: 'peru'}),
-        MyTitleFactory({title: 'semicolons are the worst', color: 'mediumaquamarine'}),
-        MyTitleFactory({title: 'jklol its okay if you like semicolons', color: 'rebeccapurple'}),
-        MyTitleFactory({title: 'im out of ideas', color: 'dodgerblue'})
-      )
-    )
-  }
+    render: function() {
+        return (
+            div(null,
+                MyTitleFactory({ title: 'props are the best', color: 'peru' }),
+                MyTitleFactory({ title: 'semicolons are the worst', color: 'mediumaquamarine' }),
+                MyTitleFactory({ title: 'jklol its okay if you like semicolons', color: 'rebeccapurple' }),
+                MyTitleFactory({ title: 'im out of ideas', color: 'dodgerblue' }),
+                MyTitleFactory({ title: 'papayawhip FTW', color: 'papayawhip' })
+            )
+        )
+    }
 })
 
 ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
