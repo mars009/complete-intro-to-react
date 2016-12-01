f66e3b15a833bdd1fd03940238190d0f0ff999d1
mcamara <mcamara@qbp.com>
2016-12-01 15:51:06 -0600

    Add PropTypes to ShowCard

--- js/Search.js
+++ js/Search.js
@@ -3,12 +3,13 @@ import preload from '../public/data.json'
 import ShowCard from './ShowCard'
 
 const Search = React.createClass({
+
     render() {
         return (
             <div className='search'>
                 {preload.shows.map((show) => {
                     return (
-                       <ShowCard key={show.imdbID} show={show}/>
+                       <ShowCard key={show.imdbID} {...show}/>
                     )
                 })}                
             </div>
