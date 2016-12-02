df18efb35ee5e216bd5eab2aa80cc3179a8cb913
mcamara <mcamara@qbp.com>
2016-12-02 10:33:44 -0600

    Add Link around ShowCard for routing

--- js/Header.js
+++ js/Header.js
@@ -2,14 +2,26 @@ import React from 'react'
 import { Link } from 'react-router'
 
 const { func, bool, string } = React.PropTypes
-Header.propTypes = {
-  handleSearchTermChange: func,
-  showSearch: bool,
-  searchTerm: string
-}
+
 
 class Header extends React.Component {
   render () {
+
+    // Contains 1 of two different components
+    let utilSpace
+
+    if (this.props.showSearch) {
+      utilSpace = <input onChange={this.props.handleSearchTermChange} value={this.props.searchTerm} type='text' placeholder='Search' />
+    } else {
+      utilSpace = (
+        <h2>
+          <Link to='/search'>
+            Back
+          </Link>
+        </h2>
+      )
+    }
+
     return (
       <header>
         <h1>
@@ -17,9 +29,16 @@ class Header extends React.Component {
             svideo
           </Link>
         </h1>
+        {utilSpace}
       </header>
     )
   }
+}
+
+Header.propTypes = {
+  handleSearchTermChange: func,
+  showSearch: bool,
+  searchTerm: string
 }
 
 export default Header
