0f59e5e9ea983ef9698a64f05dad65e5fcf03d21
mcamara <mcamara@qbp.com>
2017-06-07 22:54:30 -0500

    Add jsx to ClientApp.jsx

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
-
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'House of Cards', color: 'peru' })
+  const style = { color: props.color };
+  return (
+    <div>
+      <h1 style={style}>{props.title}</h1>
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+const MyFirstComponent = function() {
+  return (
+    <div id="my-first-component">
+      <MyTitle title="Game of Thrones" color="YellowGreen" />
+      <MyTitle title="Stranger Things" color="GreenYellow" />
+      <MyTitle title="House of Cards" color="Peru" />
+      <MyTitle title="The Americans" color="burlywood" />
+    </div>
+  );
+};
+
+render(<MyFirstComponent />, document.getElementById('app'));
