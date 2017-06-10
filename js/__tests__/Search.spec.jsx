cf641c12e921d1f974414e5d6574795b7accc87a
mars009 <marzicle@gmail.com>
2017-06-10 18:31:03 -0500

    Add enzyme for testing. Add jest.snapshotSerializers:[jest-serializer-enzyme] prop in package.json. Update Search.spec to use enzyme

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import Search from '../Search';
+import preload from '../../data.json';
+// Enzyme is a wrapper around react-test-renderer, that will stubout all the child components
+// from a Component. That way things in parent components will fail if something changed in them
+// rather than failing when their children change
+import { shallow } from 'enzyme';
+import ShowCard from '../ShowCard';
 
 test('Search renders correctly', () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const component = shallow(<Search />);
+  expect(component).toMatchSnapshot();
+});
+
+test('Search should render correct amount of shows', () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+});
+
+test('Search should render correct amount of shows based on searchTerm', () => {
+  const searchWord = 'black';
+  const component = shallow(<Search />);
+  // Find the input and simulate as if the user entered the search word
+  component.find('input').simulate('change', { target: { value: searchWord } });
+
+  // Get the show count by filtering on the searchTerm
+  const showCount = preload.shows.filter(
+    show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0
+  ).length;
+
+  expect(component.find(ShowCard).length).toEqual(showCount);
 });
